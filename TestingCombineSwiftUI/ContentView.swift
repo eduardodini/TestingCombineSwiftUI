4607050c5e37132b7648e0b2e7b34dbb0fc30459
Eduardo <ediasdini@apple.com>
2022-05-13 10:51:08 -0300

    Initial Commit

---
+++ TestingCombineSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestingCombineSwiftUI
+//
+//  Created by Eduardo Dini on 13/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TestingCombineSwiftUI/TestingCombineSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingCombineSwiftUIApp.swift
+//  TestingCombineSwiftUI
+//
+//  Created by Eduardo Dini on 13/05/22.
+//
+
+import SwiftUI
+
+@main
+struct TestingCombineSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
